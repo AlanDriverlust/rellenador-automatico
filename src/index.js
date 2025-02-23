ea1fe4e331cae1971918ca0f2cb5e1922ddd95eb
AlanDriverlust <alanpereznona@gmail.com>
2025-02-23 17:43:28 +0100

    Update index.js
    edt

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './styles.css';
 import RellenadorAutomatico from './App';
 
-// ✅ Afegeix aquesta línia per veure si React es carrega correctament
 console.log("React està funcionant!");
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
