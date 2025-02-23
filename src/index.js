5a67fbace155fcf4ac0cc7a89f3fd64ef98afa63
AlanDriverlust <alanpereznona@gmail.com>
2025-02-23 17:41:27 +0100

    Update index.js
    edit index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './styles.css';
 import RellenadorAutomatico from './App';
 
+console.log("React està funcionant!");
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
